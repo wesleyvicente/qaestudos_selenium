702df970ac7049e311b66c29aa82919a6e341448
wesleyvicente <wesleydossantosvicente@yahoo.com.br>
2021-06-26 18:03:02 -0300

    Primeira Classe

---
+++ src/test/java/qaestudos_selenium/PrimeiraClasseSelenium.java
@@ -0,0 +1,15 @@
+package qaestudos_selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PrimeiraClasseSelenium {
+	@Test
+	public void testelogin() {
+
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.com.br/");
+
+	}
+
+}
